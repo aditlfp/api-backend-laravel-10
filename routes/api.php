bdd10ddae4f2233dc17beaf025a85b8ed1312f56
aditlfp <aditya.budilfp11@gmail.com>
2023-07-13 09:58:21 +0700

    "Update User Controller"

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PesananController;
 use App\Http\Controllers\PesananUserController;
 use App\Http\Controllers\SatuanController;
 use App\Http\Controllers\StatusController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,11 +22,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::middleware('auth')->group(function() {
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::resource('/pesanan-user', PesananUserController::class);
 });
 
